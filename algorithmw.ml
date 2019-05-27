20b85cc2d8917a8b670bf0befe58f1e445cee111
Christian Scott <christianfscott@gmail.com>
2019-05-28 01:03:23 +1000

    add generalize and unify

--- algorithmw.ml
+++ algorithmw.ml
@@ -1,3 +1,5 @@
+let printf = Printf.printf
+
 open Base
 
 let flip f x y = f y x
@@ -51,10 +53,9 @@ end
 
 module Types_scheme : Types with type t := scheme = struct
   let free_type_vars s =
-    let (vars, t) = s in
+    let (_, t) = s in
     let ftv1 = Types_concrete_type.free_type_vars t in
-    let ftv2 = Set.of_list (module String) vars in
-    Set.diff ftv1 ftv2
+    ftv1
 
   let apply subst s = 
     let (vars, t) = s in
@@ -62,12 +63,37 @@ module Types_scheme : Types with type t := scheme = struct
     (vars, (Types_concrete_type.apply subst t))
 end
 
-module Types_list (T : Types) : Types = struct
-  type t = T.t list
-  
-  let free_type_vars ts =
-    let ftvs = List.map ~f:T.free_type_vars ts in
-    List.fold_right ~f:Set.union ~init:(Set.empty (module String)) ftvs
+module Subst = struct 
+  let null = Map.empty (module String)
 
-  let apply subst = List.map ~f:(T.apply subst)
+  let compose (s1 : subst) (s2 : subst) =
+    let subst = Map.map s2 ~f:(Types_concrete_type.apply s1) in 
+    Map.fold subst ~init:s1 ~f:(fun ~key ~data s ->
+      match Map.add s ~key ~data with
+      | `Ok (a) -> a
+      | `Duplicate -> s)
 end
+
+type type_env = scheme Map.M(String).t
+
+module Types_type_env : Types with type t := type_env = struct
+  let free_type_vars env =
+    let values = Map.data env in
+    let ftvs = List.map values ~f:Types_scheme.free_type_vars in
+    List.fold ftvs ~f:Set.union ~init:(Set.empty (module String))
+
+  let apply subst env = Map.map ~f:(Types_scheme.apply subst) env
+end
+
+let generalize env t : scheme =
+  let ftv_t = Types_concrete_type.free_type_vars t in
+  let ftv_env = Types_type_env.free_type_vars env in
+  let vars = Set.to_list (Set.diff ftv_t ftv_env) in
+  (vars, t)
+
+let rec unify t1 t2 = match (t1, t2) with
+| (TFunction (l1, r1), TFunction (l2, r2)) ->
+  let subst1 = unify l1 l2 in
+  let subst2 = unify (Types_concrete_type.apply subst1 r1) (Types_concrete_type.apply subst1 r2) in
+  Subst.compose subst1 subst2
+| (TInt, TInt)|(TBool, TBool)|_ -> Subst.null
