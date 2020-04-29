f5e4403efb2c374efd853db95f6321a8e477565f
Christian Scott <christian.s@canva.com>
2020-04-29 10:08:52 +1000

    format

--- src/algorithmw.ml
+++ src/algorithmw.ml
@@ -1,8 +1,6 @@
 open Base
 
-type literal =
-  | LInt of int
-  | LBool of bool
+type literal = LInt of int | LBool of bool
 
 type expression =
   (* true, 1 *)
@@ -46,20 +44,21 @@ end
 
 module Types_concrete_type : Types with type t := concrete_type = struct
   let rec free_type_vars = function
-  | TVariable (name) -> Set.singleton (module String) name
-  | TFunction (t1, t2) ->
-    let ftv1 = free_type_vars t1 in
-    let ftv2 = free_type_vars t2 in
-    Set.union ftv1 ftv2
-  | TInt|TBool -> Set.empty (module String)
+    | TVariable name -> Set.singleton (module String) name
+    | TFunction (t1, t2) ->
+        let ftv1 = free_type_vars t1 in
+        let ftv2 = free_type_vars t2 in
+        Set.union ftv1 ftv2
+    | TInt | TBool -> Set.empty (module String)
 
-  let rec apply subst t = match t with
-  | TVariable (name) ->
-    (match Map.find subst name with
-    | Some (t_nested) -> t_nested
-    | None -> TVariable name)
-  | TFunction (t1, t2) -> TFunction ((apply subst t1), (apply subst t2))
-  | TInt|TBool -> t
+  let rec apply subst t =
+    match t with
+    | TVariable name -> (
+        match Map.find subst name with
+        | Some t_nested -> t_nested
+        | None -> TVariable name )
+    | TFunction (t1, t2) -> TFunction (apply subst t1, apply subst t2)
+    | TInt | TBool -> t
 end
 
 module Subst = struct
@@ -68,25 +67,25 @@ module Subst = struct
 
   (* merge two substitutions *)
   let compose (s1 : subst) (s2 : subst) =
-    let subst = Map.map s2 ~f:(Types_concrete_type.apply s1) in 
+    let subst = Map.map s2 ~f:(Types_concrete_type.apply s1) in
     Map.fold subst ~init:s1 ~f:(fun ~key ~data s ->
-      match Map.add s ~key ~data with
-      | `Ok (a) -> a
-      | `Duplicate -> s)
+        match Map.add s ~key ~data with
+        | `Ok a -> a
+        | `Duplicate -> s)
 end
 
 module Types_scheme : Types with type t := scheme = struct
   let free_type_vars s =
-    let (_, t) = s in
+    let _, t = s in
     let ftv1 = Types_concrete_type.free_type_vars t in
     ftv1
 
-  let apply subst s = 
+  let apply subst s =
     let flip f x y = f y x in
-    let (vars, t) = s in
+    let vars, t = s in
     (* remove all variables in the scheme from subst before applying *)
     let subst = List.fold_right vars ~f:(flip Map.remove) ~init:subst in
-    (vars, (Types_concrete_type.apply subst t))
+    (vars, Types_concrete_type.apply subst t)
 end
 
 (* also called a "context" *)
@@ -124,59 +123,72 @@ let instantiate (vars, t) =
 
 exception FooBarRenameMe of string
 
-let is_var var t = match t with
-| TVariable (tt) -> String.equal var tt
-| _ -> false
+let is_var var t =
+  match t with
+  | TVariable tt -> String.equal var tt
+  | _ -> false
 
 let var_bind var t =
-  if is_var var t then
-    Subst.null
+  if is_var var t then Subst.null
   else
     let ftvs = Types_concrete_type.free_type_vars t in
-    match (Set.find ftvs ~f:(String.equal var)) with
-    | Some (_) -> raise (FooBarRenameMe "AHHHH")
+    match Set.find ftvs ~f:(String.equal var) with
+    | Some _ -> raise (FooBarRenameMe "AHHHH")
     | None -> Map.singleton (module String) var t
 
-let rec unify t1 t2 = match (t1, t2) with
-| (TFunction (l1, r1), TFunction (l2, r2)) ->
-  let subst1 = unify l1 l2 in
-  let subst2 = unify (Types_concrete_type.apply subst1 r1) (Types_concrete_type.apply subst1 r2) in
-  Subst.compose subst1 subst2
-| (TInt, TInt)|(TBool, TBool)|_ -> Subst.null
+let rec unify t1 t2 =
+  match (t1, t2) with
+  | TFunction (l1, r1), TFunction (l2, r2) ->
+      let subst1 = unify l1 l2 in
+      let subst2 =
+        unify
+          (Types_concrete_type.apply subst1 r1)
+          (Types_concrete_type.apply subst1 r2)
+      in
+      Subst.compose subst1 subst2
+  | TInt, TInt | TBool, TBool | _ -> Subst.null
 
 exception UndefinedVariable of string
 
 (* infer the type of an expression using a type environment *)
-let rec infer (env: type_env) (expr: expression) : (subst * concrete_type) = match expr with
-| ELiteral (LInt _) -> (Subst.null, TInt)
-| ELiteral (LBool _) -> (Subst.null, TBool)
-| EVariable (var) -> (match Map.find env var with
-    | Some (scheme) -> (Subst.null, instantiate scheme)
-    | None -> raise (UndefinedVariable "could not find given variable"))
-| ELambda (binder, body) ->
-    let binder_t = TypeGenerator.next () in
-    let next_env = (match Map.add env ~key:binder ~data:([], binder_t) with
-    | `Ok (e) -> e
-    | `Duplicate -> env) in
-    let (subst, body_t) = infer next_env body in
-    (subst, TFunction (binder_t, body_t))
-| ELet (x, expr, body) ->
-    let (subst1, expr_t) = infer env expr in
-    let next_env = (match Map.add env ~key:x ~data:([], expr_t) with
-    | `Ok (e) -> e
-    | `Duplicate -> env) in
-    let (subst2, body_t) = infer (Types_type_env.apply subst1 next_env) body in
-    let merged_subst = Subst.compose subst1 subst2 in
-    (merged_subst, body_t)
-| EApplication (func, arg) ->
-    let result_t = TypeGenerator.next () in
-    let (_, func_t) = infer env func in
-    let (_, arg_t) = infer env arg in
-    let unified = unify func_t arg_t in
-    (unified, result_t)
+let rec infer (env : type_env) (expr : expression) : subst * concrete_type =
+  match expr with
+  | ELiteral (LInt _) -> (Subst.null, TInt)
+  | ELiteral (LBool _) -> (Subst.null, TBool)
+  | EVariable var -> (
+      match Map.find env var with
+      | Some scheme -> (Subst.null, instantiate scheme)
+      | None -> raise (UndefinedVariable "could not find given variable") )
+  | ELambda (binder, body) ->
+      let binder_t = TypeGenerator.next () in
+      let next_env =
+        match Map.add env ~key:binder ~data:([], binder_t) with
+        | `Ok e -> e
+        | `Duplicate -> env
+      in
+      let subst, body_t = infer next_env body in
+      (subst, TFunction (binder_t, body_t))
+  | ELet (x, expr, body) ->
+      let subst1, expr_t = infer env expr in
+      let next_env =
+        match Map.add env ~key:x ~data:([], expr_t) with
+        | `Ok e -> e
+        | `Duplicate -> env
+      in
+      let subst2, body_t = infer (Types_type_env.apply subst1 next_env) body in
+      let merged_subst = Subst.compose subst1 subst2 in
+      (merged_subst, body_t)
+  | EApplication (func, arg) ->
+      let result_t = TypeGenerator.next () in
+      let _, func_t = infer env func in
+      let _, arg_t = infer env arg in
+      let unified = unify func_t arg_t in
+      (unified, result_t)
 
-let rec string_of_type t = match t with
-| TInt -> "int"
-| TBool -> "bool"
-| TVariable (var) -> var
-| TFunction (t1, t2) -> Printf.sprintf "%s -> %s" (string_of_type t1) (string_of_type t2)
+let rec string_of_type t =
+  match t with
+  | TInt -> "int"
+  | TBool -> "bool"
+  | TVariable var -> var
+  | TFunction (t1, t2) ->
+      Printf.sprintf "%s -> %s" (string_of_type t1) (string_of_type t2)
--- test/test_algorithmw.ml
+++ test/test_algorithmw.ml
@@ -1,29 +1,34 @@
 open Algorithmw
 
-type test_result =
-  | Failure
-  | Success
+type test_result = Failure | Success
 
 let () =
   let int_literal = ELiteral (LInt 0) in
   let bool_literal = ELiteral (LBool true) in
-  let cases = [
+  let cases =
+    [
       (int_literal, TInt);
       (bool_literal, TBool);
-      (ELambda ("x", int_literal), TFunction (TVariable ("a"), TInt));
-  ] in
+      (ELambda ("x", int_literal), TFunction (TVariable "a", TInt));
+    ]
+  in
   let check (expr, t) =
-    let (_, inferred) = infer Subst.null expr in
+    let _, inferred = infer Subst.null expr in
     if not (inferred = t) then
-      let _ = Printf.printf "expected %s, got %s\n" (string_of_type t) (string_of_type inferred) in
+      let _ =
+        Printf.printf "expected %s, got %s\n" (string_of_type t)
+          (string_of_type inferred)
+      in
       Failure
-    else
-      Success
+    else Success
   in
   let results = List.map check cases in
   let succeeded = List.filter (fun result -> result == Success) results in
-  if (List.length succeeded) == (List.length results) then
+  if List.length succeeded == List.length results then
     Printf.printf "all tests passed"
   else
-    let _ = Printf.printf "%d tests out of %d tests failed\n" (List.length succeeded) (List.length results) in
+    let _ =
+      Printf.printf "%d tests out of %d tests failed\n" (List.length succeeded)
+        (List.length results)
+    in
     failwith "test suite failed"
